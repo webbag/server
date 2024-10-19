a6a65d63236678744b39303bade998ee0b5b8dec
Krzysztof Kromolicki <k.kromolicki@gmail.com>
2024-10-19 22:38:35 +0200

    ubuntu

---
+++ security-ubuntu.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+# Skrypt konfiguracyjny dla Ubuntu 24 - aktualizacja, zmiana portu SSH, instalacja Fail2ban i podstawowa konfiguracja
+
+# Funkcja do sprawdzania czy komenda została wykonana poprawnie
+function check_success {
+    if [ $? -ne 0 ]; then
+        echo "Wystąpił błąd. Sprawdź logi i spróbuj ponownie."
+        exit 1
+    fi
+}
+
+# Sprawdzenie, czy podano odpowiednią liczbę argumentów lub --help
+if [[ "$1" == "--help" ]]; then
+    echo "Użycie:"
+    echo "$0 <nazwa_użytkownika> <port_ssh>"
+    echo "Przykład: $0 webbag 2222"
+    exit 0
+else then
+    echo "Błędne użycie. Parametry <nazwa_użytkownika> i <port_ssh> są wymagane. Użyj --help, aby uzyskać informacje na temat poprawnego użycia."
+    exit 1
+fi
+
+# Zmienne z nazwą użytkownika do utworzenia i portem SSH
+NEW_USER="$1"
+NEW_SSH_PORT="$2"
+
+# Aktualizacja systemu operacyjnego
+echo "Aktualizacja systemu operacyjnego..."
+sudo apt update && sudo apt upgrade -y
+check_success
+
+# Sprawdzanie, czy podany port SSH jest używany
+echo "Sprawdzanie, czy port $NEW_SSH_PORT jest używany..."
+if sudo ss -tuln | grep -q ":$NEW_SSH_PORT"; then
+  echo "Port $NEW_SSH_PORT jest już zajęty. Skrypt zakończony."
+  exit 1
+else
+  echo "Port $NEW_SSH_PORT jest wolny. Używanie tego portu dla SSH."
+fi
+
+# Edycja pliku konfiguracyjnego ssh.socket dla systemu Ubuntu 23.04 i nowszych wersji (Socket)
+UBUNTU_VERSION=$(lsb_release -rs)
+if (( $(echo "$UBUNTU_VERSION >= 23.04" | bc -l) )); then
+  echo "Wykryto wersję Ubuntu >= 23.04, aktualizacja pliku ssh.socket..."
+  if sudo grep -q "^ListenStream=22" /lib/systemd/system/ssh.socket; then
+    sudo sed -i "s/^ListenStream=22/ListenStream=$NEW_SSH_PORT/" /lib/systemd/system/ssh.socket
+    check_success
+  else
+    echo "ListenStream=22 nie został znaleziony w pliku ssh.socket, dodawanie nowej linii..."
+    echo "ListenStream=$NEW_SSH_PORT" | sudo tee -a /lib/systemd/system/ssh.socket > /dev/null
+    check_success
+  fi
+  sudo systemctl daemon-reload
+  check_success
+  sudo systemctl restart sshd
+  check_success
+else
+  # Zmiana domyślnego portu SSH
+  echo "Zmiana domyślnego portu SSH na $NEW_SSH_PORT..."
+  if sudo grep -q "^#Port 22" /etc/ssh/sshd_config; then
+    sudo sed -i "s/^#Port 22/Port $NEW_SSH_PORT/" /etc/ssh/sshd_config
+    check_success
+  elif sudo grep -q "^Port 22" /etc/ssh/sshd_config; then
+    sudo sed -i "s/^Port 22/Port $NEW_SSH_PORT/" /etc/ssh/sshd_config
+    check_success
+  else
+    echo "Port 22 nie został znaleziony w pliku konfiguracyjnym, dodawanie nowej linii..."
+    echo "Port $NEW_SSH_PORT" | sudo tee -a /etc/ssh/sshd_config > /dev/null
+    check_success
+  fi
+  echo "Restart usługi SSH..."
+  sudo systemctl restart sshd
+  check_success
+fi
+
+# Utworzenie użytkownika z ograniczonymi prawami
+if id "$NEW_USER" &>/dev/null; then
+    echo "Użytkownik $NEW_USER już istnieje."
+else
+    echo "Tworzenie nowego użytkownika: $NEW_USER..."
+    sudo adduser --disabled-password --gecos "" $NEW_USER
+    check_success
+    # Dodanie użytkownika do grupy sudo
+    sudo usermod -aG sudo $NEW_USER
+    check_success
+
+    # Konfiguracja uwierzytelniania kluczem SSH
+    sudo mkdir -p /home/$NEW_USER/.ssh
+    check_success
+    if [ -f ~/.ssh/authorized_keys ]; then
+        sudo cp ~/.ssh/authorized_keys /home/$NEW_USER/.ssh/
+        check_success
+        sudo chown $NEW_USER:$NEW_USER /home/$NEW_USER/.ssh/authorized_keys
+        check_success
+        sudo chmod 600 /home/$NEW_USER/.ssh/authorized_keys
+        check_success
+    else
+        echo "Brak pliku authorized_keys w katalogu domowym. Uwierzytelnianie kluczem SSH nie zostało skonfigurowane."
+    fi
+    sudo chown -R $NEW_USER:$NEW_USER /home/$NEW_USER/.ssh
+    check_success
+    sudo chmod 700 /home/$NEW_USER/.ssh
+    check_success
+fi
+
+# Wyłączenie logowania root przez SSH
+sudo sed -i 's/^#PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
+check_success
+
+# Instalacja Fail2ban
+echo "Instalacja Fail2ban..."
+sudo apt install fail2ban -y
+check_success
+
+# Konfiguracja Fail2ban
+echo "Konfiguracja Fail2ban..."
+sudo cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+check_success
+sudo sed -i "s/^enabled = false/enabled = true/" /etc/fail2ban/jail.local
+sudo sed -i "s/^bantime  = 10m/bantime  = 30m/" /etc/fail2ban/jail.local
+sudo sed -i "s/^maxretry = 5/maxretry = 3/" /etc/fail2ban/jail.local
+
+# Restartowanie usługi Fail2ban
+echo "Restart usługi Fail2ban..."
+sudo systemctl restart fail2ban
+check_success
+
+# Komunikat końcowy
+echo "Skrypt zakończony. Pamiętaj, aby logować się na serwer z użyciem nowego portu SSH: ssh @TwojeIP -p $NEW_SSH_PORT"
--- security.sh
+++ security.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-## Debian Security Configuration
+# Zmienna z nazwą użytkownika do utworzenia
+NEW_USER="kris"
+SSH_PORT=2222
 
 # Funkcja do sprawdzania czy komenda została wykonana poprawnie
 function check_success {
@@ -10,57 +12,36 @@ function check_success {
     fi
 }
 
-# Sprawdzenie, czy podano odpowiednią liczbę argumentów lub --help
-if [[ "$1" == "--help" ]]; then
-    echo "Użycie:"
-    echo "$0 <nazwa_użytkownika> <port_ssh>"
-    echo "Przykład: $0 webbag 2222"
-    exit 0
-elif [ $# -ne 2 ]; then
-    echo "Błędne użycie. Musisz podać nazwę użytkownika oraz port SSH. Użyj --help, aby uzyskać informacje na temat poprawnego użycia."
-    exit 1
-fi
-
-# Zmienne z nazwą użytkownika do utworzenia i portem SSH
-NEW_USER="$1"
-SSH_PORT="$2"
-
 # 1. Aktualizacja systemu
 sudo apt update && sudo apt upgrade -y
 check_success
 
 # 2. Tworzenie nowego użytkownika
-if id "$NEW_USER" &>/dev/null; then
-    echo "Użytkownik $NEW_USER już istnieje."
-else
-    sudo adduser --disabled-password --gecos "" $NEW_USER
-    check_success
-    # Dodanie użytkownika do grupy sudo
-    sudo usermod -aG sudo $NEW_USER
-    check_success
-fi
+sudo adduser --disabled-password --gecos "" $NEW_USER
+check_success
+
+# Dodanie użytkownika do grupy sudo
+sudo usermod -aG sudo $NEW_USER
+check_success
 
 # 3. Konfiguracja uwierzytelniania kluczem SSH
 sudo mkdir -p /home/$NEW_USER/.ssh
 if [ -f ~/.ssh/authorized_keys ]; then
     sudo cp ~/.ssh/authorized_keys /home/$NEW_USER/.ssh/
-    sudo chown $NEW_USER:$NEW_USER /home/$NEW_USER/.ssh/authorized_keys
-    sudo chmod 600 /home/$NEW_USER/.ssh/authorized_keys
 else
     echo "Brak pliku authorized_keys w katalogu domowym. Uwierzytelnianie kluczem SSH nie zostało skonfigurowane."
 fi
 sudo chown -R $NEW_USER:$NEW_USER /home/$NEW_USER/.ssh
 sudo chmod 700 /home/$NEW_USER/.ssh
+sudo chmod 600 /home/$NEW_USER/.ssh/authorized_keys
 
 # 4. Wyłączenie logowania root przez SSH
 sudo sed -i 's/^#PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
 check_success
 
 # 5. Zmiana domyślnego portu SSH
-if ! grep -q "^Port $SSH_PORT" /etc/ssh/sshd_config; then
-    sudo sed -i "s/^#Port 22/Port $SSH_PORT/" /etc/ssh/sshd_config
-    check_success
-fi
+sudo sed -i "s/^#Port 22/Port $SSH_PORT/" /etc/ssh/sshd_config
+check_success
 
 # Dodanie nowego portu do zapory UFW
 sudo ufw allow $SSH_PORT/tcp
@@ -108,5 +89,64 @@ check_success
 sudo systemctl restart ssh
 check_success
 
-# Podsumowanie konfiguracji bezpieczeństwa
+# 12. Instalacja i konfiguracja OpenVPN
+sudo apt install openvpn easy-rsa -y
+check_success
+
+# Tworzenie katalogu dla Easy-RSA
+easy_rsa_dir="/etc/openvpn/easy-rsa"
+sudo make-cadir $easy_rsa_dir
+check_success
+
+# Konfiguracja Easy-RSA i generowanie certyfikatów
+cd $easy_rsa_dir
+source vars
+./clean-all
+./build-ca --batch
+./build-key-server --batch server
+./build-dh
+check_success
+
+# Kopiowanie certyfikatów do katalogu OpenVPN
+sudo cp $easy_rsa_dir/keys/{server.crt,server.key,ca.crt,dh2048.pem} /etc/openvpn/
+
+# Generowanie certyfikatu i klucza dla klienta
+./build-key --batch klient
+check_success
+
+# Kopiowanie certyfikatów klienta do katalogu dostępnego dla pobrania
+sudo mkdir -p /etc/openvpn/clients
+sudo cp $easy_rsa_dir/keys/{klient.crt,klient.key,ca.crt} /etc/openvpn/clients/
+check_success
+
+# Tworzenie pliku konfiguracyjnego OpenVPN
+sudo bash -c 'cat > /etc/openvpn/server.conf <<EOF
+port 1194
+dev tun
+proto udp
+ca ca.crt
+cert server.crt
+key server.key
+dh dh2048.pem
+server 10.8.0.0 255.255.255.0
+ifconfig-pool-persist ipp.txt
+push "redirect-gateway def1 bypass-dhcp"
+push "dhcp-option DNS 8.8.8.8"
+push "dhcp-option DNS 8.8.4.4"
+keepalive 10 120
+comp-lzo
+persist-key
+persist-tun
+status openvpn-status.log
+log-append /var/log/openvpn.log
+verb 3
+EOF'
+
+# Włączenie i uruchomienie OpenVPN
+sudo systemctl enable openvpn@server
+sudo systemctl start openvpn@server
+check_success
+
+# Podsumowanie
 echo "Konfiguracja bezpieczeństwa zakończona. Zaloguj się za pomocą nowego użytkownika: $NEW_USER na porcie SSH: $SSH_PORT"
+echo "OpenVPN został zainstalowany i uruchomiony. Certyfikaty klienta dostępne w /etc/openvpn/clients. Skonfiguruj klienta VPN, aby połączyć się z serwerem."
